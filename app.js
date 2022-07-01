c288f52cab9256ebbc1c247ee8be0949120d8ddc
mdv-professional <mdv.werk@outlook.com>
2022-07-01 14:58:33 +0200

    KAS-3431 update release validations to new data model using publication activities Release decisions: Automanual

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 import { app, errorHandler } from 'mu';
 import { RELEASE_CRON_PATTERN } from './config';
 import { CronJob } from 'cron';
-import { queryMeetingsReadyForDocumentRelease, releaseDocumentsForMeeting } from './lib/query';
+import {  
+  queryMeetingsReadyForDocumentRelease, 
+  queryMeetingsReadyForThemisRelease,
+  publishDocumentsInternallyForMeetings,
+  publishDocumentsToThemisForMeetings,
+} from './lib/query';
 
-new CronJob(RELEASE_CRON_PATTERN, function() {
+new CronJob(RELEASE_CRON_PATTERN, async function() {
   console.log(`Automatic release of documents triggered by cron job at ${new Date().toISOString()}`);
-  triggerDocumentRelease();
+  await triggerInternalDocumentPublication();
+  await triggerThemisDocumentPublication()
 }, null, true);
 
-async function triggerDocumentRelease() {
+async function triggerInternalDocumentPublication() {
   const meetings = await queryMeetingsReadyForDocumentRelease();
 
   if (meetings.length) {
-    console.log(`Found ${meetings.length} meetings for which a document release must be triggered.`);
+    console.log(`Found ${meetings.length} meetings for which an internal document publication must be triggered.`);
     meetings.forEach((meeting) => {
-      console.log(`- <${meeting.uri}> doc release scheduled at ${meeting.releaseDate}`);
+      console.log(`- <${meeting.uri}> internal document publication scheduled at ${meeting.plannedPublicationTime}`);
     });
-    await releaseDocumentsForMeeting(meetings);
+    await publishDocumentsInternallyForMeetings(meetings);
     console.log(`Releasing documents finished at ${new Date().toISOString()}.`);
   } else {
-    console.log('No meeting found for which a document release must be scheduled');
+    console.log('No meeting found for which an internal document publication must be scheduled');
+  }
+};
+
+async function triggerThemisDocumentPublication() {
+  const meetings = await queryMeetingsReadyForThemisRelease();
+
+  if (meetings.length) {
+    console.log(`Found ${meetings.length} meetings for which a themis document publication must be triggered.`);
+    meetings.forEach((meeting) => {
+      console.log(`- <${meeting.uri}> themis document publication scheduled at ${meeting.plannedPublicationTime}`);
+    });
+    await publishDocumentsToThemisForMeetings(meetings);
+    console.log(`Publishing documents to themis finished at ${new Date().toISOString()}.`);
+  } else {
+    console.log('No meeting found for which a themis document publication must be scheduled');
   }
 };
 
